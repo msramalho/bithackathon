3794725faed6b8a2a530afe62e736e9db8488203
Afonso Ramos <up201506239@fe.up.pt>
2019-05-31 00:50:30 +0100

    product listing

--- frontend2/src/views/Lists.vue
+++ frontend2/src/views/Lists.vue
@@ -1,28 +1,27 @@
 <template>
-    <div>
-        Listas
+    <div class="list" ref="listing">
+        <p>Lists</p>
 
-        <Product
-        name="dog"
-        id="4949243"
-        :price='5.03'
-        />
+        <template v-for="product in products">
+            <Product :name="product.name" :price="product.price" :id="product.id" :key="product.id"></Product>
+        </template>
     </div>
 </template>
 
 <script>
-
 import Product from '../components/Product'
 
 export default {
 components: {
     Product
+},
+data() {
+    return {
+        products: [{name: "a", price: 2, id: "12"}, {name: "B", price: 69, id: "12213"}, {name: "C", price: 96.2, id: "13122"}]
+    };
 }
 };
-
 </script>
-
-
 
 <style>
 </style>
