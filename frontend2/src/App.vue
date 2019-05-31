56249bf5709f46473ca51018cedea5a6f8880957
Afonso Ramos <up201506239@fe.up.pt>
2019-05-31 12:24:32 +0100

    logo styling

--- frontend2/src/App.vue
+++ frontend2/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-    <div id="app">
-        <router-view name="header"></router-view>
-        <main>
-            <fade-transition origin="center" mode="out-in" :duration="250">
-                <router-view/>
-            </fade-transition>
-        </main>
-        <router-view name="footer"></router-view>
-    </div>
+  <div id="app">
+    <router-view name="header"></router-view>
+    <main>
+      <fade-transition origin="center" mode="out-in" :duration="250">
+        <router-view/>
+      </fade-transition>
+    </main>
+    <router-view name="footer"></router-view>
+  </div>
 </template>
+
 <script>
-import { FadeTransition } from "vue2-transitions";
-
-export default {
-  components: {
+  import {
     FadeTransition
-  }
-};
+  } from "vue2-transitions";
+  export default {
+    components: {
+      FadeTransition
+    }
+  };
 </script>
-
-<style>
-html, main, #app, main > section {
-  height: 100%;
-}
-</style>
