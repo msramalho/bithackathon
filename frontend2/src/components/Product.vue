8fee1cddb9ca3d2c144d41a361fa3c34b1ffeae2
Afonso Ramos <up201506239@fe.up.pt>
2019-05-31 01:24:51 +0100

    products listing :100:

--- frontend2/src/components/Product.vue
+++ frontend2/src/components/Product.vue
@@ -1,47 +1,49 @@
 <template>
-<div class="col-lg-3">
-    <card class="product border-3" hover shadow body-classes="py-5">
-        <img class="img-fluid" :src="'https://media.continente.pt/Sonae.eGlobal.Presentation.Web.Media/media.axd?resourceSearchType=2&resource=ProductId=' + id + '(eCsf$RetekProductCatalog$MegastoreContinenteOnline$Continente)&siteId=1&channelId=1&width=180&height=170&defaultOptions=1'" />
-        <h6 class="product-name text-primary text-uppercase">{{ name }}</h6>
-        <p class="description mt-3">{{ price }}€</p>
-        <div align-self="center">
-            <base-button tag="a" href="#" type="primary">
-                Add to List
-            </base-button>
-        </div>
-    </card>
-</div>
+    <div>
+        <card class="prod border-3" hover shadow body-classes="py-5">
+            <div class="prod-img">
+                <img class="img-fluid" :src="'https://media.continente.pt/Sonae.eGlobal.Presentation.Web.Media/media.axd?resourceSearchType=2&resource=ProductId=' + id + '(eCsf$RetekProductCatalog$MegastoreContinenteOnline$Continente)&siteId=1&channelId=1&width=180&height=170&defaultOptions=1'"
+                />
+            </div>
+            <h6 class="prod-name text-primary text-uppercase">{{ name }}</h6>
+            <p class="description mt-3">{{ price }}€</p>
+            <div align-self="center">
+                <base-button tag="a" href="#" type="primary">
+                    Add to List
+                </base-button>
+            </div>
+        </card>
+    </div>
 </template>
 
 <script>
-export default {
-    props: {
-        name: String,
-        id: String,
-        price: Number,
-    },
-
-    data() {
-        return {
-            description: 'ola',
+    export default {
+        props: {
+            name: String,
+            id: String,
+            price: Number,
+        },
+        data() {
+            return {
+                description: 'ola',
+            }
         }
     }
-    
-}
 </script>
 
 <style>
-.product {
-    margin-bottom: 2rem;
-}
-
-.product-name{
-    text-align: left;
-    padding-top: 1em;
-    font-weight: 700;
-}
-
-.description{
-    text-align: right;
-}
+    .prod {
+        margin: 0.5rem;
+    }
+    .prod-img {
+        text-align: center;
+    }
+    .prod-name {
+        text-align: left;
+        padding-top: 1em;
+        font-weight: 700;
+    }
+    .description {
+        text-align: right;
+    }
 </style>
